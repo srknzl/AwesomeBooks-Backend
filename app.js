2395f3f7f0a62d11e504650cb6d7bb19321ec3c6
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-07-07 06:29:21 +0300

    Add hostname and port variables, return Hello World html

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
+const hostname = 'localhost';
+const port = 3000;
+
 const server = http.createServer((req, res)=>{
-  console.log(req);
+  res.statusCode = 200;
+  res.setHeader('Content-Type','text/html');
+  res.end('<h1>Hello World!</h1>');
 });
 
-server.listen(8080);
-console.log(`Listening incoming connections on port ${server.address().port}`);
+server.listen(port,hostname,() => {
+console.log(`Listening incoming connections on port http://${hostname}:${port}`);
+  
+});
