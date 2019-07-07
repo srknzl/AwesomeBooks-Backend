bde8d53d896f2507501332c882de4653869719f3
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-07-07 06:31:07 +0300

    format app.js with prettier

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const http = require("http");
 
-const hostname = 'localhost';
+const hostname = "localhost";
 const port = 3000;
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type','text/html');
-  res.end('<h1>Hello World!</h1>');
+  res.setHeader("Content-Type", "text/html");
+  res.end("<h1>Hello World!</h1>");
 });
 
-server.listen(port,hostname,() => {
-console.log(`Listening incoming connections on port http://${hostname}:${port}`);
-  
+server.listen(port, hostname, () => {
+  console.log(
+    `Listening incoming connections on port http://${hostname}:${port}`
+  );
 });
