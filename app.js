7d44d22a0bd1f37130f60bd75362f553c2b79438
srknzl <serkan.ozel@boun.edu.tr>
2019-07-10 15:43:01 +0300

    add body-parser to parse a post request came through a form submission

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("This always runs");
-  next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get("/add-product", (req, res, next) => {
+  //console.log('In add product');
+  res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Submit</form>")
 });
 
-app.use("/add-product", (req, res, next) => {
-  console.log('In add product');
-  res.send("<h1>In add product page</h1>")
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use("/", (req, res, next) => {
-  console.log('In main page');
+  //console.log('In main page');
   res.send("<h1> In main page</h1>");
 });
 
