9509f9e0ee0b20278515c6d9aaeb26023c4b23ca
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-07-10 20:16:03 +0300

    use express router for admin and shop

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/add-product", (req, res, next) => {
-  //console.log('In add product');
-  res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Submit</form>")
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use("/", (req, res, next) => {
-  //console.log('In main page');
-  res.send("<h1> In main page</h1>");
+app.use(adminRouter);
+app.use(shopRouter);
+app.use('/', (req,res,next) => {
+  res.send('<h1>Not found</h1>');
 });
 
 app.listen(3000, "localhost");
