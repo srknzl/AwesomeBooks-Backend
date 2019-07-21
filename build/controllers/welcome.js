21c0c003c9eedd1f0825fdaee10a06f3f1942a6a
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-07-21 19:09:35 +0300

    delete build folder

--- build/controllers/welcome.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getWelcomePage = function (req, res, next) {
-    res.render("welcome", {
-        pageTitle: "Awesome bookstore"
-    });
-};
