b00e4f216b22d4081e43c748462b13aae96541ba
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-07-24 19:59:33 +0300

    after plain mysql2 cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { database } from "./util/database";
+import { sequelize } from "./util/database";
 
 import * as adminRoutes from "./routes/admin";
 import * as userRoutes from "./routes/user";
@@ -8,7 +8,6 @@ import * as userRoutes from "./routes/user";
 
 import * as  notFoundController from './controllers/errors';
 import * as  welcomeController from './controllers/welcome';
-import { sequelize } from "./util/database";
 
 const app = express();
 
@@ -18,9 +17,6 @@ app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-database.execute("SELECT * FROM a").then(res => {
-  console.log(res[0]);
-});
 
 app.use("/admin", adminRoutes.router);
 app.use("/user", userRoutes.router);
