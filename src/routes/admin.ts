c9725a5403469cff907c8b2821bfe24f73ac00f6
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-08-04 21:46:57 +0300

    Routes: Add validation for logins, signup add and delete product

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,16 +1,49 @@
 import * as express from "express";
+import { body } from "express-validator";
+
 import * as adminController from "../controllers/admin/adminController";
 import { isAdminAuth } from "../middleware/isAuth";
 
 export const router = express.Router();
 
-router.get("/add-product", isAdminAuth, adminController.getAddProduct);
-router.post("/add-product", isAdminAuth, adminController.postAddProduct);
+router.get("/add-product",isAdminAuth, adminController.getAddProduct);
+router.post("/add-product",[
+  body('title').isString().isLength({
+    min: 5
+  }),
+  body('price').isFloat({
+    min: 0.00,
+    max: 1000.00
+  }).isDecimal({
+    decimal_digits: "0,2"
+  }),
+  body('imageUrl').isURL(),
+  body('description').isLength({
+    min: 10,
+    max: 300
+  }).isString()
+], isAdminAuth, adminController.postAddProduct);
 
 router.get("/edit-product/:id", isAdminAuth, adminController.getEditProduct);
 router.get("/view-product/:id", isAdminAuth, adminController.getProductDetail);
 
-router.post("/edit-product", isAdminAuth, adminController.postEditProduct);
+router.post("/edit-product",[
+  body('title').isString().isLength({
+    min: 5
+  }),
+  body('price').isFloat({
+    min: 0.00,
+    max: 1000.00
+  }).isDecimal({
+    decimal_digits: "0,2"
+  }),
+  body('imageUrl').isURL(),
+  body('description').isLength({
+    min: 10,
+    max: 300
+  }).isString()
+], isAdminAuth, adminController.postEditProduct);
+
 router.post("/delete-product", isAdminAuth, adminController.postDeleteProduct);
 
 router.get("/products", isAdminAuth, adminController.getProducts);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,6 @@
 import * as express from "express";
+import { body } from "express-validator";
+
 import * as authController from "../controllers/authController";
 
 export const router = express.Router();
@@ -8,6 +10,33 @@ router.get("/signup",authController.getSignup);
 router.get("/admin-login",authController.getAdminLogin);
 
 router.post('/logout',authController.postLogout);
-router.post("/login", authController.postLogin);
-router.post("/signup",authController.postSignup);
-router.post("/admin-login",authController.postAdminLogin);
+
+router.post("/login",[
+  body('email').isEmail(),
+  body('password').isLength({
+    min: 6
+  })
+], authController.postLogin);
+
+router.post("/signup",[
+  body('email').isEmail(),
+  body('name').isString().isLength({
+    min: 3
+  }),
+  body('password').isLength({
+    min: 6
+  }),
+  body('confirmPassword').custom((value,{req}) => {
+    if(req.body.password !== value){
+      throw new Error('Passwords did not match');
+    }
+    return true;
+  })
+],authController.postSignup);
+
+router.post("/admin-login",[
+  body('email').isEmail(),
+  body('password').isLength({
+    min: 6
+  })
+],authController.postAdminLogin);
