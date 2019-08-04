c9fe3c793c04aff17371a4cc31387033d42d3240
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-08-04 13:39:56 +0300

    add mongodb store for sessions

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import { connect } from "mongoose";
 import session from "express-session";
+import flash from "connect-flash";
+import connectMongoDb from "connect-mongodb-session";
 
 import * as adminRoutes from "./routes/admin";
 import * as userRoutes from "./routes/user";
 import * as authRoutes from "./routes/auth";
 import * as homeRoutes from "./routes/home";
+import User from "./models/user";
 
 const app = express();
+const MONGODB_URI = "mongodb+srv://srknzl:PaWS1EQ7E85MHMJP@srknzl-m0-development-cluster-hgcsl.mongodb.net/learnnode-shop?retryWrites=true&w=majority";
+
+const MongoDBStore = connectMongoDb(session);
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+store.on('error',(err)=> {
+  console.error(err);
+})
 
 app.set("views", "views");
 app.set("view engine", "pug");
@@ -20,9 +34,21 @@ app.use(
     secret:
       "UlEuikYPJivAXVMR5KUX24bDU202JkQgv0QrctQRRFEIHrktim1yMgWTI3gwKhbXsnWiNfufv",
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
   })
 );
+app.use(async (req,res,next) => {
+  if(req.session && req.session.user){
+    try {
+      req.session.user = await User.findById(req.session.user._id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  next();
+});
+app.use(flash());
 
 app.use("/admin", adminRoutes.router);
 app.use("/user", userRoutes.router);
@@ -30,7 +56,7 @@ app.use(authRoutes.router);
 app.use(homeRoutes.router);
 
 connect(
-  "mongodb+srv://srknzl:PaWS1EQ7E85MHMJP@srknzl-m0-development-cluster-hgcsl.mongodb.net/learnnode-shop?retryWrites=true&w=majority",
+  MONGODB_URI,
   async err => {
     if (err) console.error(err);
     app.listen(3000, "localhost");
