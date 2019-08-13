0c32a0076bc3f56d6a4e92b4c7728c84fd4ff584
Serkan Ozel <serkan.ozel@boun.edu.tr>
2019-08-13 14:10:59 +0300

    Feature: add async delete without reloading

--- public/js/admin/products.js
+++ public/js/admin/products.js
@@ -1,8 +1,34 @@
-const cardElements = document.querySelectorAll(".card");
-cardElements.forEach((element)=>{
-  const productIdInput = element.querySelector("#productId");
-  element.addEventListener("click",()=>{
-    window.location = "/admin/view-product/"+ productIdInput.value;
+const cardImageElements = document.querySelectorAll(".card__image");
+cardImageElements.forEach(element => {
+  const productIdInput = element.parentElement.querySelector("#productId");
+  element.addEventListener("click", () => {
+    window.location = "/admin/view-product/" + productIdInput.value;
   });
 });
+const deleteProduct = btn => {
+  const csrf = btn.parentElement.querySelector("input[name='_csrf']").value;
+  const id = btn.parentElement.querySelector("input[name='id']").value;
   
+  fetch("product/delete/" + id, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrf
+    }
+  })
+    .then(res => {
+      console.log(res);
+      if(res.status === 200){
+        const article = btn.closest("article");
+        article.parentElement.removeChild(article);
+      }else{
+        alert("Could not delete the product please try again!");
+      }
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
