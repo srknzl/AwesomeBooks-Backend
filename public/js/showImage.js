fce2f1cb03b2774103ae9bd97bf264e8459fa90c
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-09-20 18:15:09 +0300

    Script: add max width and height rule to prevImage 300px

--- public/js/showImage.js
+++ public/js/showImage.js
@@ -3,16 +3,22 @@ function showImage() {
   var image = document.getElementById("image").files[0];
   var reader = new FileReader();
   reader.onload = function(e) {
+    const prevImage = document.querySelector("#previewImage");
+    if (prevImage) prevImage.parentNode.removeChild(prevImage);
+
+    const prevImageLabel = document.querySelector("#previewImageLabel");
+    if (prevImage) prevImageLabel.parentNode.removeChild(prevImageLabel);
+
     var newImage = document.createElement("img");
+    newImage.style.maxHeight = "300px";
+    newImage.style.maxWidth = "300px";
+    newImage.id = "previewImage";
+    newImage.src = e.target.result;
+    
     var imageLabel = document.createElement("p");
     imageLabel.innerHTML = "Preview image:";
     imageLabel.id = "previewImageLabel";
-    newImage.id = "previewImage";
-    newImage.src = e.target.result;
-    const prevImage = document.querySelector("#previewImage");
-    if (prevImage) prevImage.parentNode.removeChild(prevImage);
-    const prevImageLabel = document.querySelector("#previewImageLabel");
-    if (prevImage) prevImageLabel.parentNode.removeChild(prevImageLabel);
+
     imageSelector.parentNode.insertBefore(
       imageLabel,
       imageSelector.nextSibling
