8942c9b4ab90d7db61c844a9802766e36d6c4fa8
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-10-26 14:05:40 +0300

    Feature: Add dummy book detail page

---
+++ frontend/src/views/BookDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="bookdetail">
+      {{id}}
+  </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
