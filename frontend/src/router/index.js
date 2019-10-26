18a0bfebbb3d677722571cdbb5c935277d79d414
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-10-26 14:05:16 +0300

    Router: Add new routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../views/Home";
+import Shop from "../views/Shop";
+import BookDetail from "../views/BookDetail";
+
 
 Vue.use(VueRouter);
 
@@ -10,14 +13,24 @@ const routes = [
     name: "home",
     component: Home
   },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/shop",
+    name: "shop",
+    component: Shop
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    component: BookDetail
   }
 ];
 
