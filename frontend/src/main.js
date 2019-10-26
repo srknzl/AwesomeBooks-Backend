d20e079cd9c905d8a5238d80498733668a42d0b1
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-10-26 14:03:42 +0300

    Feature: Add imports for bootstrap into main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,21 @@
 import Vue from "vue";
 import App from "./App.vue";
+import VueSidebarMenu from "vue-sidebar-menu";
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import BootstrapVue from 'bootstrap-vue'
+import "@fortawesome/fontawesome-free/css/all.css";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import '../src/styles/global.scss'
+// library.add(faUserSecret)
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.use(VueSidebarMenu);
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
