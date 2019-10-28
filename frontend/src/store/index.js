dc9f32bae3450101d5f4edf97018670be17573a4
Serkan Özel <serkan.ozel@boun.edu.tr>
2019-10-28 15:43:23 +0300

    Feature: Start using vuex for backdrop collapse expand commits

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,8 +4,17 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    sidebarCollapsed: true
+  },
+  mutations: {
+    collapseSidebar: function(state){
+      state.sidebarCollapsed = true;
+    },
+    expandSidebar: function(state){
+      state.sidebarCollapsed = false;
+    }
+  },
   actions: {},
   modules: {}
 });
