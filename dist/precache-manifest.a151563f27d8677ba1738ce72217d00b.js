23926d26d174283c27c7ee10f5a4134e09e8e072
Serkan Özel <serkan.ozel@boun.edu.tr>
2020-01-24 00:02:32 +0300

    Update gitignore

---
+++ dist/precache-manifest.a151563f27d8677ba1738ce72217d00b.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17244eca2829d819bf2",
+    "url": "/css/app.067dbfb0.css"
+  },
+  {
+    "revision": "b418d3c017151bb384ac",
+    "url": "/css/chunk-vendors.38bb9130.css"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/fonts/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/fonts/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/fonts/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/fonts/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/fonts/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/fonts/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/fonts/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/fonts/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/fonts/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/fonts/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/fonts/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/fonts/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "ed59ffabbbac2bcd68341b8e6207319c",
+    "url": "/global.css"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/img/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/img/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/img/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "b59abdad90152202f6da0afc92ff5e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17244eca2829d819bf2",
+    "url": "/js/app.7621f1ef.js"
+  },
+  {
+    "revision": "b418d3c017151bb384ac",
+    "url": "/js/chunk-vendors.7637a02b.js"
+  },
+  {
+    "revision": "d1bed006787b3c34ca4524c61db5184d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "7a92085b8f281f1cfa2fd7ff6fe92fe3",
+    "url": "/styles/global.scss"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.a151563f27d8677ba1738ce72217d00b.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "awesomebookstore"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
